caa4c3b829de21640b83b45b9546e98b77f060c4
LAXMAN_RADDI <159695664+LAXMANRADDI@users.noreply.github.com>
2025-09-16 20:25:20 +0530

    Update 2leapyear.py

--- 2leapyear.py
+++ 2leapyear.py
@@ -1,6 +1,21 @@
 // to check wether given year is leap year or not , since we r using return type bool if it says true then is leap year , if says false  then it is not leap year.
+    //// passed all cases
 year = int(input())
 print(is_leap(year))
 
 def is_leap(year):
     return (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0)
+
+
+
+////////////// passed 4 out of six cases
+def is_leap(year):
+    x= year % 4 
+    y =  year % 100
+    if x == 0 and y != 0 :
+        return True
+    else:
+        return False
+
+year = int(input())
+print(is_leap(year))
