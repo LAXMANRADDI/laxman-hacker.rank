1f1dbc079c63931e34b06d70ead17755bb5d7419
LAXMAN_RADDI <159695664+LAXMANRADDI@users.noreply.github.com>
2025-09-16 21:18:28 +0530

    Update 3nospace.py

--- 3nospace.py
+++ 3nospace.py
@@ -1,3 +1,9 @@
  n = int(input())
 for i in range(1,n+1):
     print(i,end="")
+//////////////////////////////////
+//range(1, n+1) → generates numbers from 1 to n.
+
+//print(i, end="") → prints each number without a newline or space.
+
+// Default print adds a newline, but end="" overrides that.
