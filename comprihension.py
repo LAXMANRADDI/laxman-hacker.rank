ed8067051836ac1834ab847b1b124d117e3be36e
LAXMAN_RADDI <159695664+LAXMANRADDI@users.noreply.github.com>
2025-11-04 09:05:15 +0530

    Update comprihension.py

--- comprihension.py
+++ comprihension.py
@@ -25,3 +25,21 @@ print(result)
     arr = list(map(int, input().split()))
     print(sorted(set(arr))[-2])
 
+
+^^^^^^^^^^^^^^^^^^^^^^^^^ 
+hackerrank sluotion for  finding names with same score i.e second lowest score 
+
+    students = [] # empty list 
+    for _ in range(int(input())): 
+        name = input()
+        score = float(input())
+        students.append([name, score]) # storing nested lists  
+
+    
+    scores = [s[1] for s in students] # fetching scores of all students were at index 1
+    unique_scores = sorted(set(scores)) #sorting em removing dupicate values
+    second_lowest = unique_scores[1] #fetching second lowest score of athe student
+    result = [s[0] for s in students if s[1] == second_lowest] 
+    for name in sorted(result):
+        print(name)
+
