57860eab1f8495b259d65f6eb283c49e5e28e459
LAXMAN_RADDI <159695664+LAXMANRADDI@users.noreply.github.com>
2025-11-26 20:05:37 +0530

    Create slnts.py

---
+++ slnts.py
@@ -0,0 +1,8 @@
+ Command     Action                         
+ insert i e  Insert `e` at index `i`        
+print       Print the list                 
+ remove e    Remove first occurrence of `e` 
+append e   Add element at end            
+ sort       Sort list                      
+ pop         Pop last element               
+ reverse     Reverse the list              
